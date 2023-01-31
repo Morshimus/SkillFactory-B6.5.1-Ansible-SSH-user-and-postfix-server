07c067a4ce55936a33b97def0b7c9570d74fdf10
Daniel Dalavurak <morsh110792@yahoo.com>
2023-01-31 13:49:47 +0300

    ok

--- actions.ps1
+++ actions.ps1
@@ -22,12 +22,17 @@ function ansiblePlaybook {
         [Parameter(Mandatory=$False)]
         [string]$distr = "Ubuntu-20.04",
         [Parameter(Mandatory=$False)]
-        [String]$user = "morsh92",
-        [Parameter(Mandatory=$False,Position=0)]
-        [String]$args
-
+        [String]$user = "morsh92",        
+        [Parameter(Mandatory=$False)]
+        [String]$server = "lemp",
+        [Parameter(Mandatory=$False)]
+        [String]$invFile = "./yandex_cloud.ini",
+        [Parameter(Mandatory=$False)]
+        [String]$privateKey = "~/.ssh/morsh_bastion_SSH",
+        [Parameter(Mandatory=$False)]
+        [String]$Playbook = "./provisioning_web_admin.yaml"
     )
-    wsl -d $distr -u $user -e ansible-playbook "$args"
+    wsl -d $distr -u $user -e ansible-playbook  -i "$invFile" --private-key $privateKey  $Playbook 
 } 
 
 
