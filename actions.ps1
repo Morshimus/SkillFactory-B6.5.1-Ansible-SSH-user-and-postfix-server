969a7c4c1b8621177cf1c3f79c9cb04837c30484
Daniel Dalavurak <morsh110792@yahoo.com>
2023-01-31 13:36:00 +0300

    Added ansible playbook for user creation As encrypted vault file.

--- actions.ps1
+++ actions.ps1
@@ -4,10 +4,16 @@ function ansible {
         [string]$distr = "Ubuntu-20.04",
         [Parameter(Mandatory=$False)]
         [String]$user = "morsh92",
+        [Parameter(Mandatory=$False)]
+        [String]$server = "lemp",
+        [Parameter(Mandatory=$False)]
+        [String]$invFile = "./yandex_cloud.ini",
+        [Parameter(Mandatory=$False)]
+        [String]$privateKey = "~/.ssh/morsh_bastion_SSH",
         [Parameter(Mandatory=$False,Position=0)]
         [String]$args
     )
-    wsl -d $distr -u $user -e ansible $args
+    wsl -d $distr -u $user -e ansible $server --inventory-file "$invFile" --private-key $privateKey $args
 } 
 
 Set-Alias ansible-playbook ansiblePlaybook
@@ -21,7 +27,7 @@ function ansiblePlaybook {
         [String]$args
 
     )
-    wsl -d $distr -u $user -e ansible-playbook $args
+    wsl -d $distr -u $user -e ansible-playbook "$args"
 } 
 
 
